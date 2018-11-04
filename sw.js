d869996dd71256bb524e4d4fbbe41633f9fa73d0
Tindu Edward <tindu.edward@gmail.com>
2018-11-03 23:00:34 -0700

    Changes to sw

--- sw.js
+++ sw.js
@@ -46,6 +46,7 @@ self.addEventListener("activate", event => {
     )
 })
 
+/*
 self.addEventListener("fetch", event => {
     if (event.request.url.startsWith(self.location.origin)) {
         event.respondWith(
@@ -58,4 +59,35 @@ self.addEventListener("fetch", event => {
             })
         );
     }
+});
+
+*/
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            }
+
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function (response) {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    var responseToCache = response.clone();
+
+                    caches.open(staticCacheName)
+                        .then(function (cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                }
+            );
+        })
+    );
 });
